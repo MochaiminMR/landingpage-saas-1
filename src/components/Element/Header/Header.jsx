e561013624e39b15806ac13b1808189e0c59f7a5
Mochaimin Muhamad Rizq <90722468+MochaiminMR@users.noreply.github.com>
2024-01-02 23:09:14 +0700

    fix

--- src/components/Element/Header/Header.jsx
+++ src/components/Element/Header/Header.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
-import { Button } from 'react-bootstrap';
+import React from "react";
+import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { CustomButton } from '../Button/Button';
+import { CustomButton } from "../Button/Button";
 
+const CustomNavbarToggle = React.forwardRef(({ onClick }, ref) => (
+  <button
+    className="navbar-toggler custom-toggler"
+    type="button"
+    onClick={onClick}
+    ref={ref}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4 8C3.45 8 3 7.55 3 7C3 6.45 3.45 6 4 6H20C20.55 6 21 6.45 21 7C21 7.55 20.55 8 20 8H4ZM4 13H20C20.55 13 21 12.55 21 12C21 11.45 20.55 11 20 11H4C3.45 11 3 11.45 3 12C3 12.55 3.45 13 4 13ZM4 18H20C20.55 18 21 17.55 21 17C21 16.45 20.55 16 20 16H4C3.45 16 3 16.45 3 17C3 17.55 3.45 18 4 18Z"
+        fill="#F5F5F5"
+      />
+    </svg>
+  </button>
+));
 
 export const Header = () => {
   return (
-    <Navbar expand="lg" className="pt-3">
+    <Navbar expand="lg" className="pt-3 text-white">
       <Container className="nav-layout layout">
         <Navbar.Brand href="#home">
           <img src="/navbar/Logo.svg" alt="" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          as={CustomNavbarToggle}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav className="gap-nav text-white">
             <Nav.Link href="#home" className="NavLink">
@@ -29,10 +53,10 @@ export const Header = () => {
               Log In
             </Nav.Link>
 
-            <CustomButton className="navbar-btn">Get Acces</CustomButton>
+            <CustomButton className="navbar-btn">Get Access</CustomButton>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
